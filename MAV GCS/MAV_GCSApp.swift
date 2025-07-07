fa9c09a4804e478c572fb03eae5bd8c069987e15
Aiden Favish <aidenfavish@gmail.com>
2025-07-07 16:04:27 -0700

    Initial Commit

---
+++ MAV GCS/MAV_GCSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MAV_GCSApp.swift
+//  MAV GCS
+//
+//  Created by Aiden Favish on 7/7/25.
+//
+
+import SwiftUI
+
+@main
+struct MAV_GCSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
